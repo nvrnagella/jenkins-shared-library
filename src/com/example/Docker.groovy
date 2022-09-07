ff558fb535cead07e65994cc8c4ad552d090b4c7
IT1089778 <nvrnagella@gmail.com>
2022-09-07 17:06:56 +0530

    commit we have add individual functions in shared library

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -2,7 +2,6 @@
 package com.example
 
 class Docker implements Serializable{
-
     def script
 
     Docker(script){
@@ -10,11 +9,15 @@ class Docker implements Serializable{
     }
     def buildDockerImage(String imageName){
         script.echo "building the docker image..."
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo',
-                passwordVariable: 'PASS', usernameVariable: 'USER')]){
-            script.sh "docker build -t $imageName ."
-            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        script.sh "docker build -t $imageName ."
+        }
+        def dockerLogin(){
+            script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo',
+                    passwordVariable: 'PASS', usernameVariable: 'USER')]){
+                script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            }
+        }
+        def dockerPush(String imageName){
             script.sh "docker push $imageName"
         }
     }
-}
